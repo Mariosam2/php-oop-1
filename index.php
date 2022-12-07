5be3589a3157ab12533b33053e104c7f2f441608
Mariosam2 <marco.mariosaa@gmail.com>
2022-12-07 15:39:24 +0100

    var dump of obj properties

--- index.php
+++ index.php
@@ -4,10 +4,30 @@ class Movie
     public $title;
     public $desc;
     public $img;
-    public function __construct($title, $desc, $img)
+    public $genre;
+    public function __construct($title, $desc, $genre, $img)
     {
         $this->title = $title;
         $this->desc = $desc;
+        $this->genre = $genre;
         $this->img = $img;
     }
+
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    public  function  getMovie()
+    {
+        foreach ($this as $value) {
+            var_dump($value);
+        }
+    }
 }
+
+$matrix = new Movie('Matrix', 'Lorem matrix', 'action', 'matrix.jpg');
+$star_wars = new Movie('Star Wars', 'Lorem star wars', 'drama', 'starwar.jpg');
+//var_dump($matrix->getImg());
+$matrix->getMovie();
+$star_wars->getMovie();
